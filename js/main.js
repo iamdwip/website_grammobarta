b806f1b50b513f27b51f629680d2b08535e8db10
Dwip Biswas <biswasdip566@gmail.com>
2021-06-22 16:02:47 +0530

    22 june news 1

--- js/main.js
+++ js/main.js
@@ -304,3 +304,19 @@ function showSlides() {
     setTimeout(showSlides, 2500);
 }
 showSlides();
+
+// // Photogaphy modal
+// $(document).ready(function() {
+//     $("img").click(function() {
+//         var t = $(this).attr("src");
+//         $(".modal-body").html("<img src='" + t + "' class='modal-img'>");
+//         $("#myModal2").modal();
+//     });
+
+//     $("video").click(function() {
+//         var v = $("video > source");
+//         var t = v.attr("src");
+//         $(".modal-body").html("<video class='model-vid' controls><source src='" + t + "' type='video/mp4'></source></video>");
+//         $("#myModal2").modal();
+//     });
+// });
